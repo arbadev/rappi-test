e0d00e26507bae3e14b843e82fcc4fbdff524320
And3 <arbadev@gmail.com>
2018-03-15 18:17:48 -0400

    added flexbox grid with css loader and semantic ui components

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 import 'normalize-css'
 
 import styles from './app.css'
-import Header from '../../components/Header'
-import Main from '../Main'
 
 class App extends Component {
   constructor() {
@@ -16,22 +15,18 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div className={styles.body}>
-          <Header />
-
-          <Switch>
-            <Route exact path="/"
-              render={
-                () => {
-                  return (
-                    <Main />
-                  )
-                }}
-            />
-          </Switch>
-        </div>
-      </Router>
+      <Grid
+        fluid
+      >
+        <Row className={styles.App}>
+          <Col md={12} lg={3}>
+            Hello, world!
+          </Col>
+          <Col md={12} lg={9}>
+            Hello, world!
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
